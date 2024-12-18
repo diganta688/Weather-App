ff9842e93d9c70b255041245f4f01b9dd5743502
Diganta <digantachakraborty688@gmail.com>
2024-12-18 23:04:08 +0530

    adding new files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import './App.css'
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <>
+      <Button variant="outlined">Outlined</Button>
+    </>
+  )
+}
+
+export default App
